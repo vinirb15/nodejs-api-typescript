63f3be02fe1b6d6618d842d5d8bc02b6e703f8d8
vbueno <viniciusbueno15@gmail.com>
2022-11-16 18:30:30 -0300

    [feat]: ts application

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import 'dotenv/config'
+import express, { Request, Response, Application } from 'express'
+import routes from './routes'
+
+const app: Application = express()
+
+app.use(express.json())
+
+// v1 routes
+app.use('/api/v1', routes)
+
+app.use('/', (req: Request, res: Response) => {
+  res.send({
+    Application: 'Node API',
+    Version: 'beta'
+  })
+})
+
+// not found
+app.use(function (req: Request, res: Response, next: any) {
+  const err = new Error('Not Found')
+  res.status(404)
+  next(err)
+})
+
+// catch all
+app.use((error: any, req: Request, res: Response) => {
+  res.status(error.status || 500)
+  res.json({ error: error.message })
+})
+
+app.listen(process.env.PORT, () => {
+  const dtNow = new Date()
+  console.log('--------------------------------------')
+  console.log(`    Server running:${dtNow.getHours()}:${dtNow.getMinutes()}:${dtNow.getSeconds()}    `)
+  console.log(`    ENV:${process.env.NODE_ENV ?? ''} PORT:${process.env.PORT ?? ''}`)
+  console.log('--------------------------------------')
+})
