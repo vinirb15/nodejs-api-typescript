2181af3dbe8494b93a644b4151aedd972646dc04
vinícius bueno <viniciusbueno15@gmail.com>
2022-11-24 14:34:01 -0300

    [fix]: seeds by model

--- src/database/seeds/movies.ts
+++ src/database/seeds/movies.ts
@@ -1,9 +1,6 @@
 import { Knex } from 'knex'
-import axios from 'axios'
-export async function seed (knex: Knex): Promise<void> {
-  // Deletes ALL existing entries
-  await knex('movies').del()
-
-  // Inserts seed entries
-  await axios.post('http://localhost:5000/api/v1/movies')
+import MoviesModel from '../../api/models/MoviesModel'
+export async function seed (_knex: Knex): Promise<void> {
+  const externalMovies: any[] = await MoviesModel.getExternalMovies()
+  await MoviesModel.fillMovies(externalMovies)
 };
